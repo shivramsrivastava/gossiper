e40e9f3893492c11abd386bc827564772332aa03
shivramsrivastava <shivram.srivastava@huawei.com>
2016-05-02 11:54:33 +0000

    Adding debug info to policy engine

--- policyengine/rulethreshold.go
+++ policyengine/rulethreshold.go
@@ -16,6 +16,6 @@ func (this *RuleThreshold) ApplyRule(policydecision *PolicyDecision) bool {
 	//set the DC thershold
 	//no chnage the dataset
 	common.ResourceThresold = this.RecosurceLimit
-	log.Println("ApplyRule: RuleThreshold setting the Threshold")
+	log.Println("ApplyRule: RuleThreshold setting the Threshold", common.ResourceThresold)
 	return true
 }
